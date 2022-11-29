467e5e4a56ceea50390f36821b4d0b3364c02867
Leopold Jacquot <leopold.jacquot@infomaniak.com>
2022-11-29 15:35:41 +0100

    fix font issue

--- webpack.config.js
+++ webpack.config.js
@@ -87,18 +87,6 @@ const config = {
           },
         }],
       },
-      {
-        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[contenthash].[ext]',
-              outputPath: 'fonts/',
-            },
-          },
-        ],
-      },
     ],
   },
   plugins: [
